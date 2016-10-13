d46b00abeed69931f5b6034818fe7c14cbfec656
duypq94 <phamquangduy9418@gmail.com>
2016-10-13 16:22:01 +0000

    login

--- db/migrate/20161013145747_create_users.rb
+++ db/migrate/20161013145747_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :password_digest
       t.string :remember_digest
       t.string :picture
       t.datetime :birthday
@@ -15,6 +13,5 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_index :users, :email, unique: true
   end
 end
